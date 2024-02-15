ffd9d44c67a60d1cd3e433dd7e9fc29826dc5523
Marcus Tastler <mtastler@t-online.de>
2024-02-15 11:04:24 +0100

    In progress...

---
+++ CS4N.EnergyHistory/Components/Contracts/IDataImport.cs
@@ -0,0 +1,9 @@
+﻿using CS4N.EnergyHistory.Contracts.Models.Data;
+
+namespace CS4N.EnergyHistory.Contracts
+{
+  public interface IDataImport
+  {
+    List<StationDataYear> ImportCsvData(string filePath);
+  }
+}
---
+++ CS4N.EnergyHistory/Components/DataImport.FritzBox/DataImport.cs
@@ -0,0 +1,13 @@
+﻿using CS4N.EnergyHistory.Contracts;
+using CS4N.EnergyHistory.Contracts.Models.Data;
+
+namespace CS4N.EnergyHistory.DataImport.FritzBox
+{
+  public sealed class DataImport : IDataImport
+  {
+    public List<StationDataYear> ImportCsvData(string filePath)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
